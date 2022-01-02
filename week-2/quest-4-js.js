9f88a14ae25f5ffd94a31899e2bd9c47b7f00fd1
kyo144 <kwsmcy@gmail.com>
2022-01-03 05:19:31 +0800

    solve: week-2 作業 JS 版 Q4 完成

--- week-2/quest-4-js.js
+++ week-2/quest-4-js.js
@@ -1,7 +1,15 @@
 // * Two Sum
+// * 挑戰用 O(n)
 
 function twoSum(nums, target){
-  
+  const map = {};
+  for (let i = 0; i < nums.length; i++) {
+    let anotherOne = target - nums[i];
+    if (map[anotherOne] !== undefined) {
+      return [map[anotherOne], i]
+    };
+    map[nums[i]] = i;
+  }
 }
 
 let result = twoSum([2, 11, 7, 15], 9);
