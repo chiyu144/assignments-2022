e13fa21c982d525ffa39ebbdc90173b58116ddcd
kyo144 <kwsmcy@gmail.com>
2022-01-04 00:48:53 +0800

    solve: 發現 Q5 寫錯 QQ，重新改好。

--- week-2/quest-5-js.js
+++ week-2/quest-5-js.js
@@ -2,17 +2,21 @@
 
 function maxZeros (nums) {
   // * O(n)
+  let count = 0;
   let result = 0;
 
-  // * 紀錄上一個遇到 1 的位置 (先判斷第一個數字是否為 1，是的話就先記著了，否的話就設 -1 代表還沒有遇到 1)
-  let lastOne = nums[0] === 1 ? 0 : -1;
+  // * 如果 0 結尾的 nums 就補個 1 在最後 (方便計算 0 的數量，以 1 跟 1 之間為標記)
+  nums[nums.length - 1] === 0 && nums.push(1);
 
-  // * 如果遇到 1，並且先前已經有遇過 1 (有上一個 1 的位置)了
-  // * 就用目前的 1 跟上一個 1 的位置相減 → 得到距離 → 跟 result 內存的數據比大小得到結果 
   for(let i = 0; i < nums.length; i++) {
-    if (nums[i] === 1 && lastOne !== -1) {
-      result = i - lastOne > result ? i - lastOne : result;
-      lastOne = i;
+    // * 如果遇到 0，就 +1 計數
+    if (nums[i] === 0) {
+      count += 1;
+    } else {
+      // * 遇到 1，就開始比大小
+      result = count > result ? count : result
+      // * 因為遇到 1 了，計數器要重新計，也已經比完大小了所以可以安心歸 0
+      count = 0;
     }
   };
 
@@ -23,3 +27,4 @@ maxZeros([0, 1, 0, 0]); // * 2
 maxZeros([1, 0, 0, 0, 0, 1, 0, 1, 0, 0]); // * 4
 maxZeros([1, 1, 1, 1, 1]); // * 0
 maxZeros([0, 0, 0, 1, 1]) // * 3
+maxZeros([1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0]) // * 5
