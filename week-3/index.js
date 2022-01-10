6979a2792244c514eae1c34baaa0eb257c08eca7
kyo144 <kwsmcy@gmail.com>
2022-01-11 03:52:42 +0800

    feature: 處理 week-3 quest-2、quest-3 skeleton 狀態

--- week-3/index.js
+++ week-3/index.js
@@ -17,8 +17,9 @@ document.addEventListener('DOMContentLoaded', async () => {
         renderArticles(articles, nextPage)
         articles.length / pageUnit <= nextPage
         ? (
+          loadMoreBtn.disabled = true ,
           loadMoreBtn.textContent = '沒有景點囉 !',
-          loadMoreBtn.disabled = true 
+          loadMoreBtn.style.cursor = 'unset'
         ) : nextPage ++
       } 
     })
@@ -51,8 +52,9 @@ const renderArticles = (articles, nextPage = 1) => {
     const article = document.createElement('article')
     const img = document.createElement('img')
     img.src = file.split('https://').map(val => 'https://' + val).slice(1)[0]
+    onImgLoaded(img)
     const div = document.createElement('div')
-    div.className = 'article-title'
+    div.classList.add('article-title')
     div.textContent = `${stitle}`
     article.appendChild(img)
     article.appendChild(div)
@@ -60,4 +62,18 @@ const renderArticles = (articles, nextPage = 1) => {
   })
 
   mainSection.appendChild(fragment)
+}
+
+// * 實作 Skeleton 用
+// * 整個流程: fetch 資料 → loading spinner 畫面 → 資料回來等圖片載入中 → Skeleton 畫面 → 圖片載好 → 正常畫面)
+const onImgLoaded = img => {
+  if (img.complete) {
+    // * 圖片已被載入 (圖片無情瞬間載好，不需做事)
+    return
+  } else {
+    // * 如果圖片還沒載好，顯示 Skeleton
+    img.classList.add('skeleton')
+    // * 圖片載好後，關掉 Skeleton
+    img.onload = () => img.classList.remove('skeleton')
+  }
 }
