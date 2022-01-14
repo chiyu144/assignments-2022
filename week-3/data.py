d8e701f3773d7eb83569ae2076532bb4e811e91a
kyo144 <kwsmcy@gmail.com>
2022-01-14 17:52:24 +0800

    style: 依照 Python 命名慣例修改變數 & 函式 & 參數名

--- week-3/data.py
+++ week-3/data.py
@@ -1,27 +1,27 @@
 from urllib.request import urlopen
 import json, csv
 
-def getSpotsData (apiUrl):
-  with urlopen(apiUrl) as res:
+def get_spots_data (api_url):
+  with urlopen(api_url) as res:
     # 取得 JSON 是哪個編碼 (這邊是 'utf-8')
     encoding = res.headers.get_content_charset()
-    jsonData = json.loads(res.read().decode(encoding))
-    return jsonData['result']['results']
+    json_data = json.loads(res.read().decode(encoding))
+    return json_data['result']['results']
 
-def getThumbnail (fileUrls):
-  fileUrlList = list(map(lambda x: 'https://' + x, fileUrls.split('https://')))[1:]
-  return fileUrlList[0]
+def get_thumbnail (file_urls):
+  file_url_list = list(map(lambda x: 'https://' + x, file_urls.split('https://')))[1:]
+  return file_url_list[0]
 
-def getDistrict (address):
+def get_district (address):
   return address[5:8]
 
-def generateSpotsCsv (spots):
-  with open('data.csv', 'w', newline='', encoding='utf-8') as csvData:
-    csvFile = csv.writer(csvData)
+def generate_spots_csv (spots):
+  with open('data.csv', 'w', newline='', encoding='utf-8') as csv_data:
+    csv_file = csv.writer(csv_data)
     for spot in spots:
       # 景點名稱, 區域, 經度, 緯度, 第一張圖檔網址
-      csvFile.writerow([spot['stitle'], getDistrict(spot['address']), spot['longitude'], spot['latitude'], getThumbnail(spot['file'])])
+      csv_file.writerow([spot['stitle'], get_district(spot['address']), spot['longitude'], spot['latitude'], get_thumbnail(spot['file'])])
 
-apiUrl = 'https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json'
-spots = getSpotsData(apiUrl)
-generateSpotsCsv(spots)
+api_url = 'https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json'
+spots = get_spots_data(api_url)
+generate_spots_csv(spots)
