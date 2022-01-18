6a9feb46456812bf05384f229f06865815b68770
kyo144 <kwsmcy@gmail.com>
2022-01-19 03:26:15 +0800

    feature: Flask 處理 2 個關於頁面導向的小細節 1. 登入後 →  使用者按上一頁 → 首頁登入表格要消失，改顯示"您已登入"字樣 2. 登入狀態 →  使用者在網址欄輸入路徑想直接跳去 error 頁 →  把他導去會員頁 待處理: 未登入狀態使用者想直接去 error 頁呢?

--- week-4/app.py
+++ week-4/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for, make_response
 import os
 
 app = Flask(__name__)
@@ -6,17 +6,22 @@ app.secret_key = os.urandom(12).hex()
 
 @app.route('/')
 def index ():
-  return render_template('index.html', header_title = '歡迎光臨，請輸入帳號密碼')
+  response = make_response(render_template('index.html', header_title = '歡迎使用系統'))
+  response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+  response.headers['Pragma'] = 'no-cache'
+  return response
 
 @app.route('/member/')
 def member ():
   if 'user_id' in session: 
-    return render_template('member.html', header_title = '歡迎光臨，這是會員頁')
+    return render_template('member.html', header_title = f"您好 {session.get('user_id')} 君，這是會員頁")
   return redirect(url_for('index'))
 
 @app.route('/error/')
 def error ():
-  return render_template('error.html', header_title = '失敗頁面')
+  if 'user_id' in session:
+    return redirect(url_for('member'))
+  return render_template('error.html', header_title = '登入失敗')
 
 @app.route('/signin', methods=['POST'])
 def signIn ():
@@ -26,18 +31,21 @@ def signIn ():
     message = None
     if user_id and password:
       if user_id == 'test' and password == 'test':
+        # 預設 31 天後自動失效
         session.permanent = True
+        # user_id 記錄起來
         session['user_id'] = user_id 
         return redirect(url_for('member'))
       else:
-        message = '帳號、或密碼輸入錯誤'
+        message = '帳號或密碼錯誤'
         return redirect(url_for('error', message = message))
     else:
-      message = '請輸入帳號、密碼'
+      message = '帳號或密碼不得為空'
       return redirect(url_for('error', message = message))
 
 @app.route('/signout')
 def signOut ():
+  # 刪除資料 + 清除 session
   session.pop('user_id', None)
   session.clear()
   return redirect(url_for('index'))
